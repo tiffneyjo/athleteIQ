da5bb1b91bc9dd5078695edee7fbe24a1d2446bf
tiffneyjo <jinb6838@gmail.com>
2026-04-12 18:20:13 -0400

    a lot of new changes to the interface

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/tracker")
+def tracker():
+    return render_template("tracker.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
